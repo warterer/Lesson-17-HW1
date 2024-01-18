18e2e99056a0b87f9f98f07bc6e9c676248b936b
areutskyi <artemik.tamik@gmail.com>
2024-01-18 22:57:31 +0200

    5 Employees

---
+++ Lesson-17-HW1/main.cpp
@@ -0,0 +1,11 @@
+#include "Employee.h"
+using namespace std;
+
+int main()
+{
+	Employee a("Smith", 1980, "Manager", 3200, "Bachelor");
+	Employee b("Doe", 1961, "DepLeader", 6000, "Bachelor");
+	Employee c("Jackson", 1983, "Promoter", 1200, "Bachelor");
+	Employee d("Canella", 1976, "Manager", 3100, "Bachelor");
+	Employee e("Jablonowski", 1959, "Lawyer", 4200, "Bachelor");
+}
